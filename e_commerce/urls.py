1e12a4a33aadb96feaeafe2cbc5782ce7b4d30e4
KADEEJA-SHAMEER <kadeejashameer110@gmail.com>
2026-01-31 13:15:10 +0530

    changes

--- e_commerce/urls.py
+++ e_commerce/urls.py
@@ -20,6 +20,6 @@ from django.urls import path,include
 from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('users/', include('userApp.urls')),
-    path('product/',include('productApp.urls')),
+    path('api/users/', include('userApp.urls')),
+    path('api/product/',include('productApp.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
